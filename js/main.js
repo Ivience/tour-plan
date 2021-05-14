00d8f45fa54f18745e1dee80adabdc8c3b635b99
Ivience <viktoriyacollinz25@gmail.com>
2021-05-14 15:09:55 +0300

    create: added reviews section

--- js/main.js
+++ js/main.js
@@ -13,3 +13,17 @@ const hotelSwiper = new Swiper('.hotel-slider', {
     onlyInViewport: false,
   },
 });
+const reviewsSwiper = new Swiper('.reviews-slider', {
+  // Optional parameters
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.reviews-slider__button--next',
+    prevEl: '.reviews-slider__button--prev',
+  },
+  keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+  },
+});
