6a3ce55d66c8e25a641a6dbadc213bcfb70ddc57
Ivience <viktoriyacollinz25@gmail.com>
2021-05-18 19:45:32 +0300

    fix: i can't understand git

---
+++ js/main.js
@@ -0,0 +1,29 @@
+const hotelSwiper = new Swiper('.hotel-slider', {
+  // Optional parameters
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.hotel-slider__button--next',
+    prevEl: '.hotel-slider__button--prev',
+  },
+  effect: "coverflow",
+  keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+  },
+});
+const reviewsSwiper = new Swiper('.reviews-slider', {
+  // Optional parameters
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.reviews-slider__button--next',
+    prevEl: '.reviews-slider__button--prev',
+  },
+  keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+  },
+});
